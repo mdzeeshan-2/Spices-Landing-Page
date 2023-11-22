ae861975e22a70fa49c19a3f3a7622006b1aab09
Md Zeeshan <98448457+mdzeeshan-2@users.noreply.github.com>
2023-11-22 09:52:28 +0530

    Add files via upload

---
+++ src/pages/Spice.js
@@ -0,0 +1,269 @@
+import "./Spice.css";
+
+const Spice = () => {
+  return (
+    <div className="spice">
+      <img className="icon" alt="" src="/11112222-1@2x.png" />
+      <div className="spice-child" />
+      <div className="spice-child" />
+      <div className="spice-inner" />
+      <div className="rectangle-div" />
+      <div className="div">
+        <div className="div-child" />
+        <img className="image-icon" alt="" src="/image@2x.png" />
+        <div className="div1">
+          <div className="div2">
+            <img className="svg-icon" alt="" src="/svg.svg" />
+            <div className="div3">
+              <div className="video" />
+              <div className="div4">
+                <div className="svg" />
+                <div className="h3">
+                  <div className="break-your-goals">break your goals</div>
+                  <div className="br">
+                    <p className="blank-line">&nbsp;</p>
+                  </div>
+                  <div className="into-actionable">into actionable</div>
+                  <div className="br1">
+                    <p className="blank-line">&nbsp;</p>
+                  </div>
+                  <div className="span">
+                    <div className="spice-x0">spice x0</div>
+                  </div>
+                </div>
+                <div className="choose-a-life">
+                  choose a life goal and get a personalized checklist to
+                  complete it or create your own!
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="div5">
+            <img className="svg-icon1" alt="" src="/svg.svg" />
+            <div className="div6">
+              <div className="video1" />
+              <div className="div7">
+                <div className="svg1" />
+                <div className="h31">
+                  <div className="span1">
+                    <div className="spice-x1">spice x1</div>
+                  </div>
+                  <div className="br2">
+                    <p className="blank-line">&nbsp;</p>
+                  </div>
+                  <div className="your-life-and">
+                    your life and stop procastinating.
+                  </div>
+                </div>
+                <div className="track-your-to-dos-container">
+                  <span className="naturally-connected-txt-container">
+                    <span>{`track your `}</span>
+                    <span className="to-dos-get-reminders">
+                      to-dos, get reminders, content and tools,
+                    </span>
+                    <span> all in one place!</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="div8">
+            <img className="svg-icon2" alt="" src="/svg.svg" />
+            <div className="div3">
+              <div className="video" />
+              <div className="div10">
+                <div className="svg2" />
+                <div className="h32">
+                  <div className="span2">
+                    <div className="spice-x2">spice x2</div>
+                  </div>
+                </div>
+                <div className="compare-by-features-container">
+                  <span className="naturally-connected-txt-container">
+                    <span>{`compare by `}</span>
+                    <span className="features-and-rating">
+                      features and rating
+                    </span>
+                    <span> to get the product that suits your need</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="div11">
+          <img className="svg-icon1" alt="" src="/svg1.svg" />
+          <div className="div12">
+            <div className="video1" />
+            <div className="div7">
+              <div className="svg1" />
+              <div className="h31">
+                <div className="span3">
+                  <div className="spice-x3">spice X3</div>
+                </div>
+                <div className="br3">
+                  <p className="blank-line">&nbsp;</p>
+                </div>
+                <div className="your-growth-as-container">
+                  <span className="naturally-connected-txt-container">
+                    <p className="blank-line">your growth as</p>
+                    <p className="blank-line"> an adult</p>
+                  </span>
+                </div>
+              </div>
+              <div className="track-your-progress-container">
+                <span className="naturally-connected-txt-container">
+                  <span>{`track your progress, `}</span>
+                  <span className="features-and-rating">complete goals</span>
+                  <span>{` and `}</span>
+                  <span className="features-and-rating">
+                    save your milestone
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="about-our">about our</div>
+        <div className="have-to-say">have to say</div>
+        <div className="span4">
+          <div className="customers">customers</div>
+        </div>
+        <div className="span5">
+          <div className="spices">spices</div>
+        </div>
+        <div className="look-what-our">{`look what our `}</div>
+      </div>
+      <div className="spices-for">spices for</div>
+      <div className="about-bhumijats-spices">About Bhumijat’s Spices</div>
+      <div className="all-foods-all-container">
+        <span className="all">{`all `}</span>
+        <span className="foods">foods</span>
+        <span className="all">, all moods</span>
+      </div>
+      <div className="for-us-at-container">
+        <span className="naturally-connected-txt-container">
+          <p className="blank-line">{`For us at Bhumijat, 'spice' is more than just a word.It's an emotion that revives exquisite feasts & aromas, festive recipes and tempting garnishes.Being a pioneer in spices, quality is of the utmost essence at Everest. It's our passion to enrich lives with our spices and complement culinary memories. Everest is India’s No.1 spice brand that is driven to season your precious moments with your families & deliver great taste!....`}</p>
+          <p className="blank-line">&nbsp;</p>
+          <p className="blank-line">
+            <a
+              className="a"
+              href="https://www.everestfoods.com/everest-overview"
+              target="_blank"
+            >
+              <span className="span6">{` `}</span>
+            </a>
+          </p>
+        </span>
+      </div>
+      <img className="rectangle-icon" alt="" src="/rectangle-65.svg" />
+      <div className="spice-child1" />
+      <div className="spice-child2" />
+      <div className="spice-child3" />
+      <div className="section">
+        <div className="div14">
+          <div className="h2">
+            <img className="svg-icon4" alt="" src="/svg2.svg" />
+            <img className="svg-icon5" alt="" src="/svg3.svg" />
+            <div className="div15">
+              <div className="span7">
+                <div className="from">from</div>
+                <div className="getting-confused">getting confused</div>
+              </div>
+              <div className="span8">
+                <div className="to-finding-a">to finding a</div>
+                <div className="perfect-spice">perfect spice</div>
+              </div>
+            </div>
+          </div>
+          <div className="we-are-here">
+            we are here with you , in this journey !
+          </div>
+        </div>
+        <div className="div16">
+          <img className="div-icon" alt="" src="/div.svg" />
+          <div className="form">
+            <div className="button">
+              <div className="div17">→</div>
+            </div>
+            <div className="explore-spices">Explore Spices</div>
+          </div>
+        </div>
+      </div>
+      <img className="suraj-chacha-3" alt="" src="/suraj-chacha-3@2x.png" />
+      <img className="group-icon" alt="" src="/group.svg" />
+      <div className="spice-child4" />
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <div className="try-us-risk-free-for-7-days-i-parent">
+          <div className="try-us-risk">
+            Try us risk free for 7 days, if you don’t love us, get your money
+            back.
+          </div>
+          <b className="naturally-connected">
+            <span className="naturally-connected-txt-container">
+              <span>{`Naturally `}</span>
+              <span className="connected">Connected</span>
+            </span>
+          </b>
+          <b className="for-a-sustainable-container">
+            <span className="naturally-connected-txt-container">
+              <span>{`for a `}</span>
+              <span className="connected">sustainable</span>
+              <span> Earth</span>
+            </span>
+          </b>
+          <img className="suraj-chacha-2" alt="" src="/suraj-chacha-2@2x.png" />
+        </div>
+        <div className="aa-1-parent">
+          <img className="aa-1-icon" alt="" src="/aa-1@2x.png" />
+          <div className="a1">
+            <img className="svg-icon6" alt="" src="/svg4.svg" />
+            <div className="bhumijat777gmailcom">bhumijat777@gmail.com</div>
+          </div>
+          <div className="a2">
+            <img className="svg-icon7" alt="" src="/svg5.svg" />
+            <div className="bhumijatagro">@bhumijatagro</div>
+          </div>
+          <div className="a3">
+            <img className="svg-icon7" alt="" src="/svg6.svg" />
+            <div className="bhumijatagro">@bhumijatagro</div>
+          </div>
+          <div className="ul">
+            <b className="how-it-works">How it works</b>
+            <b className="about">About</b>
+            <b className="our-work">Our work</b>
+            <b className="blog">Blog</b>
+            <b className="contact-us">Contact us</b>
+          </div>
+        </div>
+      </div>
+      <img className="navbar-icon" alt="" src="/navbar.svg" />
+      <div className="bmjt-logo-black-1-png-1-parent">
+        <img
+          className="bmjt-logo-black-1-png-1"
+          alt=""
+          src="/bmjt-logo-black-1-png-1@2x.png"
+        />
+        <img className="image-1-icon" alt="" src="/image-1@2x.png" />
+        <div className="hovered">
+          <div className="home">Home</div>
+          <img className="hovered-child" alt="" src="/vector-2.svg" />
+        </div>
+        <div className="about1">About</div>
+        <div className="rice">Rice</div>
+        <div className="spices1">Spices</div>
+        <div className="blogs">Blogs</div>
+      </div>
+      <div className="vector-parent">
+        <img className="group-item" alt="" src="/rectangle-71.svg" />
+        <div className="explore-screen">
+          <img className="share-08-icon" alt="" src="/svg.svg" />
+          <b className="take-a-tour">TAKE A TOUR</b>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spice;
